ea1f66a75a2bfd54a55298ac172786d623636962
prosabd <102315258+prosabd@users.noreply.github.com>
2024-09-10 12:16:19 +0200

    feat: :bulb: comments and rework main file (auto completion errors compared to fast ai course exemple)

--- 3-best-image-model/main.py
+++ 3-best-image-model/main.py
@@ -1,13 +1,34 @@
 import pandas as pd
 
+# Load ImageNet results
 df_results = pd.read_csv('pytorch-image-models/results/results-imagenet.csv')
 
+# Create a copy of the original 'model' column
 df_results['model_org'] = df_results['model']
+# Simplify model names
 df_results['model'] = df_results['model'].str.split('_').str[0]
 
 def get_data(part, col):
-    df = pd.read_csv(f'benchmark-{part}-amp-nhwc-pt111-cu113-rtx3090.csv').merge(df_results, on='model')
-    df['secs'] = 1. / df[col]
-    df['family'] = df.model.str.extract('^([a-z]+?(?:v2)?)(?:\d|_|$)')
-    df = df[~df.model.str.contains('in22'), 'family'] = df.loc[df.model.str.contains('in22'), 'family'] + '_in22'
+    # Load and merge benchmark data with ImageNet results
+    df = pd.read_csv(f'benchmark-{part}-amp-nhwc-pt112-cu113-rtx3090.csv').merge(df_results, on='model')
     
+    # Calculate time (in seconds)
+    df['secs'] = 1. / df[col]
+    
+    # Extract model 'family' from the name
+    df['family'] = df.model.str.extract('^([a-z]+?(?:v2)?)(?:\d|_|$)')
+    
+    # Remove models ending with 'gn'
+    df = df[~df.model.str.endswith('gn')]
+    
+    # Add '_in22' to the family for models containing 'in22'
+    df.loc[df.model.str.contains('in22'), 'family'] = df.loc[df.model.str.contains('in22'), 'family'] + '_in22'
+    
+    # Add 'd' to the family for ResNet models
+    df.loc[df.model.str.contains('resnet.*d'),'family'] = df.loc[df.model.str.contains('resnet.*d'),'family'] + 'd'
+    
+    # Filter the DataFrame to include only specific families
+    return df[df.family.str.contains('^re[sg]netd?|beit|convnext|levit|efficient|vit|vgg|swin')]
+
+# Get data for inference
+df = get_data('infer', 'infer_samples_per_second')
