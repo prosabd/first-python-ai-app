b7993935a09f44865ad5b803b31354fd253779f6
prosabd <102315258+prosabd@users.noreply.github.com>
2024-09-22 16:46:27 +0200

    feat: :bulb: some comments

--- 3-best-image-model/___display_data.py
+++ 3-best-image-model/___display_data.py
@@ -1,8 +1,13 @@
 import __read_data as rd
 import plotly.express as px
+
+# Define constants
 w, h = 1000, 800
 
+# Define the function to display data
 def show_all(df, title, size):
+    # Create a scatter method
     return px.scatter(df, width=w, height=h, size=df[size]**2, title=title, x='secs', y='top1', log_x=True, color='family', hover_name='model_org', hover_data=[size])
 
+# Call the function to display the data, and show it on default browser
 show_all(rd.df, 'Inference', 'infer_img_size').show()
