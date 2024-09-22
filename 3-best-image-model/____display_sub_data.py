f62c9ad94ec1c0bb969ed189650866647f46c8ce
prosabd <102315258+prosabd@users.noreply.github.com>
2024-09-22 19:48:02 +0200

    feat: :green_heart: display data between speed and parameter count colored by infer_img_size🎨

--- 3-best-image-model/____display_sub_data.py
+++ 3-best-image-model/____display_sub_data.py
@@ -15,4 +15,10 @@ def show_subs(df, title, size):
     return px.scatter(df_subs, width=w, height=h, size=df_subs[size]**2, title=title, trendline="ols", trendline_options={'log_x':True}, 
                       x='secs', y='top1', log_x=True, color='family', hover_name='model_org', hover_data=[size])
     
-show_subs(rd.df, 'Inference', 'infer_img_size').show()
+# show_subs(rd.df, 'Inference', 'infer_img_size').show()
+
+# Try displaying by speed vs parameter count with infer_img_size as color
+px.scatter(rd.df, width=w, height=h,
+    x='param_count_x',  y='secs', log_x=True, log_y=True, color='infer_img_size',
+    hover_name='model_org', hover_data=['infer_samples_per_sec', 'family']
+).show()
